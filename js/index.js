1190b2f9db360024ceedbdabcb05a3b674c132b8
DIANACF <djcarranza@uniboyaca.edu.co>
2022-09-12 20:34:29 -0500

    se agrego la funcionalidad de registrar contactos

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ function mostrarContactos(){
     const tbody = document
     .getElementById('contactosTable')
     .getElementsByTagName('tbody')[0];
+    tbody.innerHTML = ''
     for(let index in contactos){
         const contacto = contactos[index];
         const id = parseInt(index) + 1;
@@ -29,3 +30,35 @@ function mostrarContactos(){
 
 }
 mostrarContactos();
+
+document.getElementById('cerrarModalBtn').addEventListener('click', ()=>{
+    document.getElementById('formularioModal')
+    .classList.add('ocultarModal');
+
+})
+function cerrarModal(){
+    document.getElementById('formularioModal')
+        .classList.remove('ocultarModal');
+    const formulario = document.forms['formContacto'];
+    formulario.reset(); 
+
+}
+
+document.getElementById('registrarBtn').addEventListener('click', ()=>{
+    cerrarModal();
+});
+
+
+document.getElementById('formularioContacto').addEventListener('submit', (ev)=>{
+    ev.preventDefault();
+    const contacto = {
+        nombre:document.forms['formContacto']['nombre'].value,
+        email:document.forms['formContacto']['email'].value,
+        telefono:document.forms['formContacto']['telefono'].value,
+    };
+    //const nombre = document.getElementById('nombre').value;
+    //alert(nombre);
+    contactos.push(contacto);
+    cerrarModal();
+    mostrarContactos();
+});
